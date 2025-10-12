0bdddc210165946a1589666b5e000ed085a2df2b
Yudhishthir <yudhishthir@poplify.com>
2025-10-12 18:35:05 +0530

    Add failing test case - when given an empty string

--- spec/string_adder_spec.rb
+++ spec/string_adder_spec.rb
@@ -1,4 +1,10 @@
 require "string_adder"
 
 RSpec.describe StringAdder do
+  describe ".add" do
+    context "when given an empty string" do
+      it "returns 0" do
+        expect(StringAdder.add("")).to eq(0)
+      end
+    end
 end
