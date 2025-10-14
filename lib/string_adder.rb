64a913c50d065b79f9ecae76743797ba885f13ff
Yudhishthir <yudhishthir@poplify.com>
2025-10-14 21:44:58 +0530

    Add case for multiple numbers with new-lines & commas

--- lib/string_adder.rb
+++ lib/string_adder.rb
@@ -1,7 +1,7 @@
 class StringAdder
   def self.add(input)
     return 0 if input.empty?
-    numbers = input.split(",").map(&:to_i)
+    numbers = input.split(/[\n,]/).map(&:to_i)
     numbers.sum
   end
 end
